5e8da09141a902dc419885d6857815251160cb39
afilipovski <filipovskia@yahoo.com>
2023-03-04 12:51:35 +0100

    4.3.2023

--- aoc_15.py
+++ aoc_15.py
@@ -1,3 +1,6 @@
+from utils import Interval
+
+
 def manhattan(xa, ya, xb, yb):
     return abs(xa - xb) + abs(ya - yb)
 
@@ -19,7 +22,6 @@ def parse_input(file_name):
         parts = [[entry[2][2:-1], entry[3][2:-1], entry[-2][2:-1], entry[-1][2:]] for entry in parts]
         parts = [[int(p) for p in entry] for entry in parts]
         for p in parts:
-            objects.add((p[0], p[1]))
             objects.add((p[-2], p[-1]))
         sensors = [Sensor(*p) for p in parts]
     return {
@@ -32,9 +34,11 @@ example = False
 if example:
     file = 'example.txt'
     yd = 10
+    block = 20
 else:
     file = 'input.txt'
     yd = 2000000
+    block = 4000000
 
 data = parse_input(file)
 sensors = data["sensors"]
@@ -42,13 +46,36 @@ sensors = data["sensors"]
 objects = data["objects"]
 objects = set([b[0] for b in objects if b[1] == yd])
 
-intercepts = set()
-for s in sensors:
-    i = 0
-    while manhattan(s.x, s.y, s.x + i, yd) <= s.dist:
-        if s.x - i not in objects:
-            intercepts.add(s.x - i)
-        if s.x + i not in objects:
-            intercepts.add(s.x + i)
-        i = i + 1
-print("P1:", len(intercepts))
+
+def interval_at_y(y: int) -> Interval:
+    interval = Interval()
+    interval.contiguous = []
+    for s in sensors:
+        sway = s.dist - abs(y - s.y)
+        # print(sway)
+        if sway >= 0:
+            contiguous = s.x - sway, s.x + sway
+            interval.append(contiguous)
+    # print(interval)
+    return interval
+
+
+p1_int = interval_at_y(yd)
+i = len(p1_int)
+for o in objects:
+    if o in p1_int:
+        i -= 1
+print("P1:", i)
+
+p2 = 0
+for i in range(block):
+    row_int = interval_at_y(i)
+    row_int.intersection((0, block))
+    if len(row_int) != block + 1:  # found the row
+        for j in range(block):
+            if j not in row_int:
+                p2 = 4000000 * j + i
+                break
+    if p2:
+        break
+print("P2:",p2)
--- utils.py
+++ utils.py
@@ -4,3 +4,51 @@ def sign(n):
     elif n < 0:
         return -1
     return 0
+
+
+class Interval:
+    contiguous = list()
+
+    def __str__(self):
+        res = ""
+        for c in self.contiguous:
+            res += f"{c}, "
+        return res
+
+    def __len__(self):
+        res = 0
+        for c in self.contiguous:
+            res += c[1] - c[0] + 1
+        return res
+
+    def __contains__(self, item: int | float):
+        for c in self.contiguous:
+            if c[0] <= item <= c[1]:
+                return True
+        return False
+
+    def append(self, other: tuple[int | float, int | float]):
+        for c in self.contiguous:
+            if (
+                    c[0] <= other[0] <= c[1] or
+                    c[0] <= other[1] <= c[1] or
+                    (other[0] <= c[0] and c[1] <= other[1])
+            ):
+                self.contiguous.remove(c)
+                new_c = min(c[0], other[0]), max(c[1], other[1])
+                self.append(new_c)
+                return
+        self.contiguous.append(other)
+
+    def intersection(self, other: tuple[int | float, int | float]):
+        for c in self.contiguous:
+            if (
+                    c[0] <= other[0] <= c[1] or
+                    c[0] <= other[1] <= c[1] or
+                    (other[0] <= c[0] and c[1] <= other[1])
+            ):
+                self.contiguous.remove(c)
+                new_c = max(c[0], other[0]), min(c[1], other[1])
+                self.contiguous.append(new_c)
+            else:
+                self.contiguous.remove(c)
